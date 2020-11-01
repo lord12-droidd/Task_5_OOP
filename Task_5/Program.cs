8b83719f06845daaf814ecb4eaba2ecdb723e66d
Melnyk <melnikdmitriy2002@gmail.com>
2020-11-01 22:06:42 +0200

    Реалізував виведення вхідних даних та без дублювання коду

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -11,49 +11,35 @@ namespace Task_5
 
     class Program
     {
-        private static void ReadX(int n_x, double[] x_array)
+        private static void Read(int n, double[] array, string filename)
         {
-            using (var sr = new StreamReader("x.txt"))
+            using (var sr = new StreamReader(filename))
             {
+                for (int i = 0; i < n; i++)
+                {
+                    try
+                    {
+                        array[i] = Convert.ToDouble(sr.ReadLine());
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine($"Ви ввели некоректне(-ні) значення у файлі {filename}");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                    }
+                }
+                Console.WriteLine($"Елементи файлу {filename}: ");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
 
-                for (int i = 0; i< n_x; i++)
-                {
-                    try
-                    {
-                        x_array[i] = Convert.ToDouble(sr.ReadLine());
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Ви ввели некоректне(-ні) значення у файлі 'x.txt'");
-                        Console.ReadKey();
-                        Environment.Exit(0);
-                    }
-                }
-            }
-        }
-        private static void ReadY(int n_y, double[] y_array)
-        {
-            using (var sr = new StreamReader("y.txt"))
-            {
-                for (int i = 0; i < n_y; i++)
-                {
-                    try
-                    {
-                        y_array[i] = Convert.ToDouble(sr.ReadLine());
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Ви ввели некоректне(-ні) значення у файлі 'y.txt'");
-                        Console.ReadKey();
-                        Environment.Exit(0);
-                    }
-                }
-            }
-        }
 
         private static void NewX(int n_x, double[] x_array, double[] new_arrx)
         {
-            for(int i = 0; i < n_x; i++)
+            for (int i = 0; i < n_x; i++)
             {
                 if (x_array[i] % 2 != 0)
                 {
@@ -65,16 +51,16 @@ namespace Task_5
                 }
             }
         }
-        private static void FormZ(int n_x,int n_y, double[] z_array, double[] x_array, double[] y_array)
+        private static void FormZ(int n_x, int n_y, double[] z_array, double[] x_array, double[] y_array)
         {
             File.WriteAllText("z.txt", String.Empty);
-            Console.WriteLine("Елементи масиву z: ");
             if (n_y == n_x)
             {
+                Console.WriteLine("Елементи масиву z: ");
                 for (int i = 0; i < n_x; i++)
                 {
                     z_array[i] = Math.Pow(x_array[i], 2) + Math.Pow(y_array[i], 2);
-                    using (var sw = new StreamWriter("z.txt",true))
+                    using (var sw = new StreamWriter("z.txt", true))
                     {
                         Console.WriteLine($"{z_array[i]}");
                         sw.WriteLine($"{z_array[i]}");
@@ -87,71 +73,59 @@ namespace Task_5
                 Console.WriteLine("Не можна сформувати масив 'z' так як різна кількість елементів в масивах");
             }
         }
-        private static int LinesX(int n_x)
+
+
+        private static int Lines(int n, string filename)
         {
             try
             {
-                var lines = File.ReadAllLines("x.txt").Where(arg => !string.IsNullOrWhiteSpace(arg));
-                File.WriteAllLines("x.txt", lines);
-                n_x = File.ReadAllLines("x.txt").Length;
-                if(n_x == 0)
+                var lines = File.ReadAllLines(filename).Where(arg => !string.IsNullOrWhiteSpace(arg));
+                File.WriteAllLines(filename, lines);
+                n = File.ReadAllLines(filename).Length;
+                if (n == 0)
                 {
-                    Console.WriteLine("Файл 'x.txt' - пустий, будь ласка, заповніть файл 'x.txt'");
+                    Console.WriteLine($"Файл {filename} - пустий, будь ласка, заповніть файл {filename} ");
                     Console.ReadKey();
                     Environment.Exit(0);
                 }
-                return n_x;
+                return n;
             }
             catch (FileNotFoundException)
             {
-                Console.WriteLine("Будь ласка, створіть файл 'x.txt'");
+                Console.WriteLine($"Будь ласка, створіть файл {filename} ");
                 Console.ReadKey();
                 Environment.Exit(0);
             }
-            return n_x;
-        }
-        private static int LinesY(int n_y)
-        {
-            try
-            {
-                var lines = File.ReadAllLines("y.txt").Where(arg => !string.IsNullOrWhiteSpace(arg));
-                File.WriteAllLines("y.txt", lines);
-                n_y = File.ReadAllLines("y.txt").Length;
-                if (n_y == 0)
-                {
-                    Console.WriteLine("Файл 'y.txt' - пустий, будь ласка, заповніть файл 'y.txt'");
-                    Console.ReadKey();
-                    Environment.Exit(0);
-                }
-                return n_y;
-            }
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("Будь ласка, створіть файл 'y.txt'");
-                Console.ReadKey();
-                Environment.Exit(0);
-            }
-            return n_y;
+            return n;
         }
 
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
-            int n_x = 0;
-            n_x = LinesX(n_x);
-            int n_y = 0;
-            n_y = LinesY(n_y);
-            double[] x_array = new double[n_x];
-            double[] y_array = new double[n_y];
-            double[] z_array = new double[n_y];
-            ReadX(n_x, x_array);
-            ReadY(n_y,y_array);
-            for (int i = 0; i < n_x; i++)
+            Dictionary<string, int> fileLines = new Dictionary<string, int>
             {
-                NewX(n_x, x_array, x_array);
+                {"x.txt", 0},
+                {"y.txt", 0}
+            };
+            int n = 0;
+            foreach (string file in fileLines.Keys.ToList())
+            {
+                fileLines[file]=Lines(n, file);
             }
-            FormZ(n_x, n_y, z_array, x_array, y_array);
+            double[] x_array = new double[fileLines["x.txt"]];
+            double[] y_array = new double[fileLines["y.txt"]];
+            double[] z_array = new double[fileLines["y.txt"]];
+            Read(fileLines["x.txt"], x_array, "x.txt");
+            Read(fileLines["y.txt"], y_array, "y.txt");
+            Console.WriteLine($"Оновлені елементи файлу 'x.txt': ");
+            for (int i = 0; i < fileLines["x.txt"]; i++)
+            {
+                NewX(fileLines["x.txt"], x_array, x_array);
+                Console.WriteLine(x_array[i]);
+            }
+            
+            FormZ(fileLines["x.txt"], fileLines["y.txt"], z_array, x_array, y_array);
             Console.ReadKey();
         }
     }
